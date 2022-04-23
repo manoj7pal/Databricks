1539d51e77c6473df94242c67b248a895bdbae8f
Manoj Pal <48145565+manoj7pal@users.noreply.github.com>
2022-04-23 17:47:20 -0400

    Magic Commands

--- notebooks/Introduction.py
+++ notebooks/Introduction.py
@@ -1,5 +1,42 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ##Notebooks Introduction
+# MAGIC * UI Intro
+# MAGIC * Magic commands
+
+# COMMAND ----------
+
 print("Hello")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT "Hello"
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls dbfs:/databricks-datasets/COVID/USAFacts/
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC 
+# MAGIC head dbfs:/databricks-datasets/COVID/USAFacts/covid_confirmed_usafacts.csv
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
