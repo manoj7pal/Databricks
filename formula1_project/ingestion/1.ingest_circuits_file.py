2d9f30d0dc644b21c1fc88a1a820c65533d59e9e
Manoj Pal <48145565+manoj7pal@users.noreply.github.com>
2022-05-15 16:37:39 -0400

    Added - Notebook Variables via Child Notebook, Runtime Parameters, and configured the workflow

--- formula1_project/ingestion/1.ingest_circuits_file.py
+++ formula1_project/ingestion/1.ingest_circuits_file.py
@@ -11,8 +11,23 @@
 # MAGIC   1. header : to specify that the first line contains the column names.
 # MAGIC   2. inferSchema = automatically detects the correct datatype. 
 # MAGIC   3. schema = inferSchema process is inefficient, as the driver node goes through entire dataset to determine the types. To improvise, we should explicity define and change the schema while loading.
-# MAGIC   4. 
+# MAGIC 
+# MAGIC   - Including variables from other notebooks - includes/configuration and includes/common_functions
+# MAGIC   - Added Widgets -> to pass runtime notebook parameters
 # MAGIC ```
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# Adding widgets to pass runtime notebook parameters
+dbutils.widgets.text("p_datasource", "")
+v_datasource = dbutils.widgets.get("p_datasource")
+
+dbutils.widgets.text("p_env", "")
+v_env = dbutils.widgets.get("p_env")
 
 # COMMAND ----------
 
@@ -33,7 +48,8 @@ circuits_schema = StructType(
                                 ] )
 
 # Load csv file
-file = "/mnt/formula1projectstorage/raw/circuits.csv"
+file = f"{raw_dir_path}/circuits.csv"
+
 circuits_df = spark.read.csv(file , header = True, schema = circuits_schema)
 
 # COMMAND ----------
@@ -70,11 +86,14 @@ circuits_selected_df = circuits_df.select(circuits_df.circuitId, circuits_df.cir
 
 # COMMAND ----------
 
+from pyspark.sql.functions import lit
+
 circuits_renamed_df = circuits_selected_df.withColumnRenamed( "circuitId", "circuit_id" )\
     .withColumnRenamed("circuitRef", "circuit_ref")\
     .withColumnRenamed("lat", "latitude")\
     .withColumnRenamed("lng", "longitude")\
-    .withColumnRenamed("alt", "altitude")
+    .withColumnRenamed("alt", "altitude")\
+    .withColumn("datasource", lit(v_datasource))
 
 # COMMAND ----------
 
@@ -84,11 +103,17 @@ circuits_renamed_df = circuits_selected_df.withColumnRenamed( "circuitId", "circ
 
 # COMMAND ----------
 
-# adding a new column - here current_timestamp() return a column value. If we want to add a constant/literal value for each row we have wrap lit(<constant>) around the constant value.
-from pyspark.sql.functions import current_timestamp, lit
+# MAGIC %run "../includes/common_functions"
 
-circuits_final_df = circuits_renamed_df.withColumn("ingestion_date", current_timestamp())\
-                    .withColumn("env", lit("Production"))
+# COMMAND ----------
+
+# adding a new column - here current_timestamp() return a column value. If we want to add a constant/literal value for each row we have wrap lit(<constant>) around the constant value.
+# from pyspark.sql.functions import current_timestamp, lit
+
+# circuits_final_df = circuits_renamed_df.withColumn("ingestion_date", current_timestamp())\
+#                     .withColumn("env", lit("Production"))
+
+circuits_final_df = add_ingestion_date(circuits_renamed_df).withColumn("environment", lit(v_env))
 
 # COMMAND ----------
 
@@ -98,19 +123,21 @@ circuits_final_df = circuits_renamed_df.withColumn("ingestion_date", current_tim
 
 # COMMAND ----------
 
-storage_account_name = "formula1projectstorage"
-processed_path = f"/mnt/{storage_account_name}/processed/circuits"
+# storage_account_name = "formula1projectstorage"
+# processed_path = f"/mnt/{storage_account_name}/processed/circuits"
+processed_path = f"{processed_dir_path}/circuits"
+
 
 circuits_final_df.write.parquet(processed_path, mode="overwrite")
 
 # COMMAND ----------
 
-display(dbutils.fs.ls("/mnt/formula1projectstorage/processed/circuits"))
+display(dbutils.fs.ls(f"{processed_dir_path}/circuits"))
 
 # COMMAND ----------
 
 #Read the written file
-df = spark.read.parquet("/mnt/formula1projectstorage/processed/circuits")
+df = spark.read.parquet(f"{processed_dir_path}/circuits")
 display(df)
 
 # COMMAND ----------
@@ -126,3 +153,7 @@ display(df)
 # %fs
 # ls /mnt/formula1projectstorage/processed/circuits
 # display(circuits_final_df)
+
+# COMMAND ----------
+
+dbutils.notebook.exit("Success")
