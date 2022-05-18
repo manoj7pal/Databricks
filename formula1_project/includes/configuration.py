5c6d5714d5bc3c1e746e59e08a0e5d389eb4624a
Manoj Pal <48145565+manoj7pal@users.noreply.github.com>
2022-05-17 22:42:34 -0400

    Added presentation layer directory path

--- formula1_project/includes/configuration.py
+++ formula1_project/includes/configuration.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 raw_dir_path = "/mnt/formula1projectstorage/raw"
 processed_dir_path = "/mnt/formula1projectstorage/processed"
+presentation_dir_path = "/mnt/formula1projectstorage/presentation"
 
 # COMMAND ----------
 
